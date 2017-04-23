476ed30fa4e39bd797e168e0b735af36bce593f8
Nik Rahmel <niklas@rahmel.eu>
2017-04-23 18:44:42 +0100

    Refactor: Get getOAuthRequestToken for each request

--- index.js
+++ index.js
@@ -25,90 +25,83 @@ const oa = new OAuth(
   'HMAC-SHA1'
 );
 
-oa.getOAuthRequestToken((error, oAuthToken, oAuthTokenSecret, results) => {
-  app.get('/', (req, res) => {
-    const accessTokenKey = _.get(req, 'cookies.accesstoken');
-    const accessTokenSecret = _.get(req, 'cookies.accesstokensecret');
+app.get('/', (req, res) => {
+  const accessTokenKey = _.get(req, 'cookies.accesstoken');
+  const accessTokenSecret = _.get(req, 'cookies.accesstokensecret');
 
-    if (accessTokenKey && accessTokenSecret) {
-      return res.redirect('/homeTimeline')
-    } else {
-      return res.redirect('/login');
+  if (accessTokenKey && accessTokenSecret) {
+    return res.redirect('/homeTimeline')
+  } else {
+    return res.redirect('/login');
+  }
+});
+
+app.get('/login', (req, res) => {
+  async.waterfall([
+    (cb) => {
+      oa.getOAuthRequestToken(cb);
+    },
+    (oAuthToken, oAuthTokenSecret, results, cb) => {
+      const authURL = 'https://twitter.com/oauth/authenticate?oauth_token=' + oAuthToken;
+      res.redirect(authURL);
+      cb(null, null)
     }
+  ], (err, res) => {
+    if (err) return (err);
+  });
+});
+
+app.get('/callback', (req, res) => {
+  const urlObj = nodeUrl.parse(req.url, true);
+
+  async.waterfall([
+    (cb) => {
+      oa.getOAuthRequestToken(cb);
+    },
+    (oAuthToken, oAuthTokenSecret, results, cb) => {
+      oa.getOAuthAccessToken(urlObj.query.oauth_token, oAuthTokenSecret, urlObj.query.oauth_verifier, cb);
+    },
+    (oAuthAccessToken, oAuthAccessTokenSecret, result, cb) => {
+      res.cookie('accesstoken', oAuthAccessToken);
+      res.cookie('accesstokensecret', oAuthAccessTokenSecret);
+      res.redirect('/homeTimeline');
+      cb(null, null);
+    },
+  ], (err, results) => {
+    if (err) return err;
+  });
+});
+
+app.get('/homeTimeline', (req, res) => {
+
+  const accessTokenKey = _.get(req, 'cookies.accesstoken');
+  const accessTokenSecret = _.get(req, 'cookies.accesstokensecret');
+
+  if (!accessTokenKey && !accessTokenSecret) return res.redirect('/');
+
+  const client = new Twitter({
+    consumer_key: twitterConsumerKey,
+    consumer_secret: twitterConsumerSecret,
+    access_token_key: accessTokenKey,
+    access_token_secret: accessTokenSecret
   });
 
-  app.get('/login', (req, res) => {
-    const urlObj = nodeUrl.parse(req.url, true);
-    const authURL = 'https://twitter.com/' +
-      'oauth/authenticate?oauth_token=' + oAuthToken;
+  client.get('statuses/home_timeline', {
+    count: 200
+  }, (error, tweets, response) => {
+    if (error) throw error;
 
-    res.redirect(authURL);
+    let tweetview = '<html><head></head><body><ul>';
+    for (const tweet of tweets) {
+      tweetview += `<li><strong>${tweet.user.screen_name}:</strong> ${tweet.text}</li>`
+    }
+
+    tweetview += '</ul></body></html>'
+    res.end(tweetview);
   });
 
-  app.get('/callback', (req, res) => {
-    const urlObj = nodeUrl.parse(req.url, true);
-    const getOAuthRequestTokenCallback = (error, oAuthAccessToken,
-      oAuthAccessTokenSecret, results) => {
-      if (error) {
-        console.log(error);
-        res.end(JSON.stringify({
-          message: 'Error occured while getting access token',
-          error: error
-        }));
-        return;
-      }
+})
 
-      oa.get('https://api.twitter.com/1.1/account/verify_credentials.json',
-        oAuthAccessToken,
-        oAuthAccessTokenSecret,
-        (error, twitterResponseData, result) => {
-          if (error) {
-            console.log(error)
-            res.end(JSON.stringify(error));
-            return;
-          }
-          res.cookie('accesstoken', oAuthAccessToken);
-          res.cookie('accesstokensecret', oAuthAccessTokenSecret);
-          res.redirect('/homeTimeline');
-        });
-    };
-
-    oa.getOAuthAccessToken(urlObj.query.oauth_token, oAuthTokenSecret,
-      urlObj.query.oauth_verifier,
-      getOAuthRequestTokenCallback);
-  });
-
-  app.get('/homeTimeline', (req, res) => {
-
-    const accessTokenKey = _.get(req, 'cookies.accesstoken');
-    const accessTokenSecret = _.get(req, 'cookies.accesstokensecret');
-
-    if (!accessTokenKey && !accessTokenSecret) return res.redirect('/');
-
-    const client = new Twitter({
-      consumer_key: twitterConsumerKey,
-      consumer_secret: twitterConsumerSecret,
-      access_token_key: accessTokenKey,
-      access_token_secret: accessTokenSecret
-    });
-
-    client.get('statuses/home_timeline', {
-      count: 200
-    }, (error, tweets, response) => {
-      if (error) throw error;
-
-      let tweetview = '<html><head></head><body><ul>';
-      for (const tweet of tweets) {
-        tweetview += `<li><strong>${tweet.user.screen_name}:</strong> ${tweet.text}</li>`
-      }
-
-      tweetview += '</ul></body></html>'
-      res.end(tweetview);
-    });
-
-  })
-
-  app.listen(3000, () => {
-    console.log('Example app listening on port 3000!')
-  });
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!')
 });
