c25bbdc8922b46f36f53f3f0b5ed7b05a5f0167d
Nik Rahmel <niklas@rahmel.eu>
2017-04-26 22:00:42 +0100

    Display tweets sing jquery

--- public/js/homeTimeline.js
+++ public/js/homeTimeline.js
@@ -1,21 +1,28 @@
-var socket = io('/homeTimeline');
+$(document).ready(function () {
+  var socket = io('/homeTimeline');
 
-var accessTokenKey = Cookies.get('accesstoken');
-var accessTokenSecret = Cookies.get('accesstokensecret');
+  var accessTokenKey = Cookies.get('accesstoken');
+  var accessTokenSecret = Cookies.get('accesstokensecret');
 
-socket.emit('event', {
-  type: 'auth',
-  accessTokenKey: accessTokenKey,
-  accessTokenSecret: accessTokenSecret
-});
-
-socket.on('event', (data) => {
-  switch (data.type) {
-    case 'tweet':
-      console.log('got tweet', data.tweet)
-      break;
-
-    default:
-      break;
+  function addTweet(tweet) {
+    $('#xtc-tweets').prepend('<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3"> <div class="card xtc-tweet-card mt-3"> <div class="card-block"><h4 class="card-title">' + tweet.user.screen_name + '</h4><p class="card-text">' + tweet.text + '</p><a href="#" class="btn btn-primary">Shortlist</a></div></div></div>');
   }
-})
+
+  socket.emit('event', {
+    type: 'auth',
+    accessTokenKey: accessTokenKey,
+    accessTokenSecret: accessTokenSecret
+  });
+
+  socket.on('event', (data) => {
+    switch (data.type) {
+      case 'tweet':
+        addTweet(data.tweet);
+        break;
+
+      default:
+        break;
+    }
+  })
+
+});
